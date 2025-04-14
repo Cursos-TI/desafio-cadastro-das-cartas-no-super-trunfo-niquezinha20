aeb3773972c5ac711eb3e995239c08b574b459fa
niquezinha20 <moniquesouzas559@gmail.com>
2025-04-14 13:24:44 -0300

    Add files via upload

---
+++ supertrunfo.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+
+int main(){
+char estado1[50];
+char nomecidade1[50];
+ int população1 ;
+ float área1;
+float pib1;
+ int ponto1;
+
+
+
+ printf("digite o estado:");
+ scanf("%s", &estado1);
+
+ printf("o nome da cidade:");
+ scanf("%s", &nomecidade1);
+
+
+
+ printf("população: ");
+ scanf("%d",  &população1);
+
+ printf ("Área em km: ");
+ scanf ("%e", &área1);
+
+ printf ("PIB: ");
+ scanf ("%e", &pib1);
+
+
+ printf ("número de ponto turístico: ");
+ scanf ("%d", &ponto1);
+
+
+
+ printf (" carta 1\n");
+
+
+
+printf ("estado: %s \n",estado1 );
+printf ("cidade: %s\n",nomecidade1);
+printf ("população: %d \n",população1);
+printf ("Área em km: %e \n",área1);
+printf ("pib: %e \n",pib1);
+printf ("número de ponto turístico: %d\n",ponto1);
+
+
+char estado2[50];
+char codigo2[50];
+char nomecidade2[50];
+ int populacao2 ;
+ float area2;
+float pib2;
+ int ponto2;
+
+
+ printf("digite o segundo estado:");
+ scanf("%s", &estado2);
+
+ printf("o nome da cidade:");
+ scanf("%s", &nomecidade2);
+
+ printf("população: ");
+ scanf("%d",  &populacao2);
+
+ printf ("Área em km: ");
+ scanf ("%e", &area2);
+
+ printf ("PIB: ");
+ scanf ("%e", &pib2);
+
+ printf ("número de ponto turístico: ");
+ scanf ("%d", &ponto2);
+
+
+
+ printf (" carta 2\n");
+
+
+printf ("estado: %s \n",estado2 );
+printf ("cidade: %s\n",nomecidade2);
+printf ("população: %d \n",populacao2);
+printf ("Área em km: %e \n",area2);
+printf ("pib: %e \n",pib2);
+printf ("número de ponto turístico: %d\n",ponto2);
+
+
+return 0;
+
+
+
+
+
+
+
+};
+
+
+
+
